a7edec92a84db7a852bd6607101334adb478a71d
Peter <peterhsaxton@gmail.com>
2014-05-15 10:06:07 +0100

    default point

--- spec/point_spec.js
+++ spec/point_spec.js
@@ -1,17 +1,24 @@
 describe('Point', function(){
+  var Pt = Hammerhead.Point;
   describe('initialisation', function(){
     it('from two coordinates', function(){
-      pt = Hammerhead.Point(2, 3);
+      pt = Pt(2, 3);
       expect(pt.x).toEqual(2);
       expect(pt.y).toEqual(3);
     });
 
     it('should be imutable', function(){
-      pt = Hammerhead.Point(2, 3);
+      pt = Pt(2, 3);
       pt.x = 5;
       pt.y = 6;
       expect(pt.x).toEqual(2);
       expect(pt.y).toEqual(3);
+    });
+
+    it('should default to the origin point', function(){
+      pt = Pt();
+      expect(pt.x).toEqual(0);
+      expect(pt.y).toEqual(0);
     });
   });
 });
--- src/point.js
+++ src/point.js
@@ -1,6 +1,6 @@
 (function(parent){
   function create(x, y){
-    return Object.freeze({x: x, y: y});
+    return Object.freeze({x: x|0, y: y|0});
   }
   parent.Point = create;
 }(Hammerhead));
