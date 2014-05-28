241793c4b6f3df7c1dd5b63f55425456baa0fafc
Peter <peterhsaxton@gmail.com>
2014-05-28 18:27:32 +0100

    build

--- dist/HammerHead2.js
+++ dist/HammerHead2.js
@@ -274,40 +274,40 @@ Hammerhead = {};
   parent.Controller = create;
 }(Hammerhead));
 
-_.now = Date.now || function() { return new Date().getTime(); };
+// _.now = Date.now || function() { return new Date().getTime(); };
 
-_.debounce = function(func, wait, immediate) {
-  var timeout, args, context, timestamp, result;
+// _.debounce = function(func, wait, immediate) {
+//   var timeout, args, context, timestamp, result;
 
-  var later = function() {
-    var last = _.now() - timestamp;
-    if (last < wait) {
-      timeout = setTimeout(later, wait - last);
-    } else {
-      timeout = null;
-      if (!immediate) {
-        result = func.apply(context, args);
-        context = args = null;
-      }
-    }
-  };
+//   var later = function() {
+//     var last = _.now() - timestamp;
+//     if (last < wait) {
+//       timeout = setTimeout(later, wait - last);
+//     } else {
+//       timeout = null;
+//       if (!immediate) {
+//         result = func.apply(context, args);
+//         context = args = null;
+//       }
+//     }
+//   };
 
-  return function() {
-    context = this;
-    args = arguments;
-    timestamp = _.now();
-    var callNow = immediate && !timeout;
-    if (!timeout) {
-      timeout = setTimeout(later, wait);
-    }
-    if (callNow) {
-      result = func.apply(context, args);
-      context = args = null;
-    }
+//   return function() {
+//     context = this;
+//     args = arguments;
+//     timestamp = _.now();
+//     var callNow = immediate && !timeout;
+//     if (!timeout) {
+//       timeout = setTimeout(later, wait);
+//     }
+//     if (callNow) {
+//       result = func.apply(context, args);
+//       context = args = null;
+//     }
 
-    return result;
-  };
-};
+//     return result;
+//   };
+// };
 
 (function(parent){
   var $window = $(window);
