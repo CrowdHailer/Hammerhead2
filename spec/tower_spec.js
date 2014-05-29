c57711af3fc336fee0234189664222f03d503808
Peter <peterhsaxton@gmail.com>
2014-05-29 13:33:41 +0100

    full tower check

--- spec/tower_spec.js
+++ spec/tower_spec.js
@@ -1,18 +1,18 @@
 describe('tower', function(){
-  var tower;
+  var tower, dummy, dummy2;
   beforeEach(function(){
     tower = Tower.getTower();
-  });
-  it('should publish events to the subscribed handler', function(){
     dummy = jasmine.createSpy();
+    dummy2 = jasmine.createSpy();
+  });
+
+  it('should publish events to the subscribed handler', function(){
     tower.subscribe('channel1')(dummy);
     tower.publish('channel1')({x: 1});
     expect(dummy).toHaveBeenCalledWith({x: 1});
   });
 
   it('should publish events to all the subscribed handlers', function(){
-    dummy = jasmine.createSpy();
-    dummy2 = jasmine.createSpy();
     tower.subscribe('channel1')(dummy);
     tower.subscribe('channel1')(dummy2);
     tower.publish('channel1')({x: 1});
@@ -23,5 +23,16 @@ describe('tower', function(){
   it('should create a singleton instance of tower', function(){
     var otherTower = Tower.getTower();
     expect(otherTower).toBe(tower);
+  });
+
+  it('should publish events to only the correct channel', function(){
+    tower.subscribe('channel1')(dummy);
+    tower.subscribe('channel2')(dummy2);
+    tower.publish('channel1')({x: 1});
+    expect(dummy).toHaveBeenCalledWith({x: 1});
+    expect(dummy2).not.toHaveBeenCalledWith({x: 1});
+    tower.publish('channel2')({x: 2});
+    expect(dummy).not.toHaveBeenCalledWith({x: 2});
+    expect(dummy2).toHaveBeenCalledWith({x: 2});
   });
 });
