f2eb510aa41c9c543d557fa0e82b933d01b16866
Peter <peterhsaxton@gmail.com>
2014-05-30 08:23:26 +0100

    stocked changes to work with svgpoint

---
+++ src/svgPoint.js
@@ -0,0 +1,102 @@
+(function(parent){
+  "use strict";
+  function create(x, y){
+    if (isObj(x)) {
+      if (x.x) {
+        return createFromCoordinate(x);
+      } else if (x.pageX) {
+        return createFromPagePoint(x);
+      } else if (x.deltaX) {
+        return createFromDisplacementVector(x);
+      }
+    }
+    return Object.freeze({x: x || 0, y: y || 0});
+  }
+
+  function createFromCoordinate(point){
+    return create(point.x, point.y);
+  }
+
+  function createFromPagePoint(pagePoint){
+    return create(pagePoint.pageX, pagePoint.pageY);
+  }
+
+  function createFromDisplacementVector(pagePoint){
+    return create(pagePoint.deltaX, pagePoint.deltaY);
+  }
+
+  function add(p){
+    return function(q){
+      return create(p.x + q.x, p.y + q.y);
+    };
+  }
+
+  function subtract(p){
+    return function(q){
+      return create(p.x - q.x, p.y - q.y);
+    };
+  }
+
+  function negate(p){
+    return function(q){
+      q = q || create();
+      return create(q.x - p.x, q.y - p.y);
+    };
+  }
+
+  function scalar(a){
+    return function(q){
+      return create(a * q.x, a * q.y);
+    };
+  }
+
+  function min(p){
+    return function(q){
+      var x = (p.x < q.x)? p.x : q.x;
+      var y = (p.y < q.y)? p.y : q.y;
+      return create(x, y);
+    };
+  }
+
+  function max(p){
+    return function(q){
+      var x = (p.x > q.x)? p.x : q.x;
+      var y = (p.y > q.y)? p.y : q.y;
+      return create(x, y);
+    };
+  }
+
+  var darkSVG = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+
+  var svgPoint = darkSVG.createSVGPoint();
+
+  var mt = svgPoint.matrixTransform;
+
+  console.log(svgPoint);
+
+  function matrixTransform(m){
+    return function(q){
+      var newp = mt.apply(svgPoint, [m]);
+      console.log(newp);
+    //   var x = m.a * q.x + m.c * q.y + m.e;
+    //   var y = m.b * q.x + m.d * q.y + m.f;
+    //   return create(x, y);
+    };
+  }
+
+  var methodsToExtend = _.extend({
+    createFromCoordinate: createFromCoordinate,
+    createFromPagePoint: createFromPagePoint,
+    createFromDisplacementVector: createFromDisplacementVector,
+    add: add,
+    subtract: subtract,
+    negate: negate,
+    scalar: scalar,
+    min: min,
+    max: max,
+    matrixTransform: matrixTransform
+  });
+
+  methodsToExtend(create);
+  parent.Point = create;
+}(Hammerhead));
