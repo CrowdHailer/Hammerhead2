4f7b59cce46743453504db06005d89824d6dc663
Peter <peterhsaxton@gmail.com>
2014-05-31 07:48:20 +0100

    moved interpolate define to prefix

--- dist/HammerHead2.js
+++ dist/HammerHead2.js
@@ -1,3 +1,15 @@
+// String interpolations
+
+function interpolate(s) {
+  var i = 0;
+  return function(args){
+    return s.replace(/%(?:\(([^)]+)\))?([%diouxXeEfFgGcrs])/g, function (match, v, t) {
+      if (t == "%") return "%";
+      return args[v || i++];
+    });
+  };
+}
+
 Hammerhead = {};
 
 (function(parent){
--- src/prefix.js
+++ src/prefix.js
@@ -1,1 +1,13 @@
+// String interpolations
+
+function interpolate(s) {
+  var i = 0;
+  return function(args){
+    return s.replace(/%(?:\(([^)]+)\))?([%diouxXeEfFgGcrs])/g, function (match, v, t) {
+      if (t == "%") return "%";
+      return args[v || i++];
+    });
+  };
+}
+
 Hammerhead = {};
