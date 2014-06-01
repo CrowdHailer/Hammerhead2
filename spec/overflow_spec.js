07cb085f5f82fae9406a8c5a2908cd5005baf702
Peter <peterhsaxton@gmail.com>
2014-06-01 16:29:02 +0100

    calls parent element

--- spec/overflow_spec.js
+++ spec/overflow_spec.js
@@ -1,8 +1,15 @@
 describe('managing overflow padding of active elements', function(){
-  it('should subscribe to the tower', function(){
+  xit('should subscribe to the tower', function(){
     var tower = Belfry.getTower();
     spyOn(tower, 'publish');
     Hammerhead.regulateOverflow();
     expect(tower.publish).toHaveBeenCalledWith('windowResize');
+  });
+  it('should set a elements margin outside the parent', function(){
+    var spy = jasmine.createSpy();
+    spy.parent = function(){};
+    spyOn(spy, 'parent');
+    Hammerhead.regulateOverflow(spy);
+    expect(spy.parent).toHaveBeenCalled();
   });
 });
--- src/overflow.js
+++ src/overflow.js
@@ -1,6 +1,12 @@
 (function(parent){
   var tower = Belfry.getTower();
+
+  function createOverflowUpdater($element){
+    $parent = $element.parent();
+  }
   parent.regulateOverflow = function(element){
     tower.publish('windowResize');
+
+    var update = createOverflowUpdater(element)
   };
 }(Hammerhead));
