a9dc27904b265a0df0c7cfc1ca9b1f23026eb440
Peter <peterhsaxton@gmail.com>
2014-06-01 22:44:25 +0100

    test when starting off element

--- spec/touch_spec.js
+++ spec/touch_spec.js
@@ -1,25 +1,38 @@
 describe('dispatch touch notifications', function(){
   // console.log(hammertime) -very busy all elements not evenHandlers array;
-  var testSVG, testPath, preventDefault, defaultGesture;
+  var testSVG, testPath, preventDefault, defaultGesture, dummy, tower, emitter;
   beforeEach(function(){
+    // setting up DOM
     preventDefault = function(){};
     svgString = '<svg id="test" width="500" viewBox="0 0 2000 1000"><path id="test-path"></path></svg>';
     document.body.innerHTML += svgString;
-    testSVG = document.getElementById('test');
-    testPath = document.getElementById('test-path');
-    defaultGesture = {target: testPath, preventDefault: preventDefault};
+    testSVG = $('#test');
+    testPath = $('#test-path');
+
+    // test apparatus
+    tower = Belfry.getTower();
+    dummy = jasmine.createSpy();
+    defaultGesture = {target: testPath[0], preventDefault: preventDefault};
+
+    // hammerhead instance
+    emitter = Hammerhead.touchDispatch(testSVG);
   });
 
   afterEach(function(){
-    testSVG.parentElement.removeChild(testSVG);
+    testSVG.remove();
+    emitter.deactivate();
   });
 
   it('should call start on touch event over path', function(){
-    Hammerhead.touchDispatch($('svg#test'));
-    var dummy = jasmine.createSpy();
-    var tower = Belfry.getTower();
     tower.subscribe('start')(dummy);
     hammertime.trigger('touch', defaultGesture);
     expect(dummy).toHaveBeenCalledWith($('svg#test')[0], 'start');
+  });
+
+  it('should not call start misses target', function(){
+    tower.subscribe('start')(dummy);
+    var gesture = _.foundation(defaultGesture)({target: 'not-element'});
+    hammertime.trigger('touch', gesture);
+    expect(dummy).not.toHaveBeenCalled();
   });
 });
--- src/touch.js
+++ src/touch.js
@@ -63,6 +63,7 @@
     };
 
     instance.activate();
+    return instance;
 
   };
 }(Hammerhead));
