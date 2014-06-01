b5d3892cd4d640b73d5b1243c1926ac67ae601f2
Peter <peterhsaxton@gmail.com>
2014-06-01 15:22:59 +0100

    SVGroovy added to dependencies

---
+++ bower_components/SoVeryGroovy/karma.conf.js
@@ -0,0 +1,24 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'bower_components/cumin/dist/cumin.js',
+      'src/prefix.js',
+      'src/point.js',
+      'src/matrix.js',
+      'spec/point_spec.js',
+      'spec/matrix_spec.js'
+    ],
+    exclude: [
+    ],
+    reporters: ['spec'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    captureTimeout: 60000,
+    singleRun: true
+  });
+};
