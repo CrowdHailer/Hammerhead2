7f1c0e3606d52de76445226edcd41a7e6d1f58b2
Peter <peterhsaxton@gmail.com>
2014-06-03 17:46:11 +0100

    overflow margin set with pixels

--- fullfat/HammerHead2.js
+++ fullfat/HammerHead2.js
@@ -769,7 +769,7 @@ var Hammerhead = {};
     return function(){
       var height = $parent.height();
       var width = $parent.width();
-      $element.css('margin', -height/2 + ' ' + -width/2);
+      $element.css('margin', interpolate('-%(height)spx -%(width)spx')({height: height/2, width: width/2}));
       $element.width(width * 2);
       $element.height(height * 2);
     };
--- src/overflow.js
+++ src/overflow.js
@@ -6,7 +6,7 @@
     return function(){
       var height = $parent.height();
       var width = $parent.width();
-      $element.css('margin', -height/2 + ' ' + -width/2);
+      $element.css('margin', interpolate('-%(height)spx -%(width)spx')({height: height/2, width: width/2}));
       $element.width(width * 2);
       $element.height(height * 2);
     };
