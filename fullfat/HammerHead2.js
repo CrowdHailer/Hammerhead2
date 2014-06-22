2c9641b89b5fd536646759074a458087a5311862
Peter <peterhsaxton@gmail.com>
2014-06-22 15:41:37 +0100

    added resize delay to configuration

--- fullfat/HammerHead2.js
+++ fullfat/HammerHead2.js
@@ -788,10 +788,13 @@ var Hammerhead = {};
   var tower = Belfry.getTower();
 
   var buildConfig = _.foundation({
-    overflowSurplus: 0.5
+    overflowSurplus: 0.5,
+    resizeDelay: 200
   });
 
   var marginTemp = interpolate('-%(height)spx -%(width)spx');
+
+  $(window).on('resize', tower.publish('windowResize'));
 
   function createOverflowUpdater($element, options){
     var config = buildConfig(options);
@@ -800,21 +803,15 @@ var Hammerhead = {};
     var factor = 2 * surplus + 1;
     var $parent = $element.parent();
 
-    return function(){
+    return _.debounce(config.resizeDelay)(function(){
       var height = $parent.height();
       var width = $parent.width();
       $element
         .css('margin', marginTemp({height: height * surplus, width: width * surplus}))
         .width(width * factor)
         .height(height * factor);
-    };
+    });
   }
-
-  var publishResize = _.debounce(200)(tower.publish('windowResize'));
-
-  $(window).on('resize', function(){
-    publishResize();
-  });
 
   parent.regulateOverflow = function(element, options){
     var updateOverflow = createOverflowUpdater(element, options);
