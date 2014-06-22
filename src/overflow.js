05db2ba61dc11c6d767b1be6d8aa296e999e10e2
Peter <peterhsaxton@gmail.com>
2014-06-22 09:23:22 +0100

    overflow width optional

--- src/overflow.js
+++ src/overflow.js
@@ -1,14 +1,22 @@
 (function(parent){
   var tower = Belfry.getTower();
 
-  function createOverflowUpdater($element){
-    $parent = $element.parent();
+  var buildConfig = _.foundation({
+    surplus: 0.5
+  });
+
+  function createOverflowUpdater($element, options){
+    config = buildConfig(options);
+
+    var factor = 2 * config.surplus + 1;
+    console.log(factor);
+    var $parent = $element.parent();
     return function(){
       var height = $parent.height();
       var width = $parent.width();
       $element.css('margin', interpolate('-%(height)spx -%(width)spx')({height: height/2, width: width/2}));
-      $element.width(width * 2);
-      $element.height(height * 2);
+      $element.width(width * factor);
+      $element.height(height * factor);
     };
   }
 
@@ -18,8 +26,8 @@
     publishResize();
   });
 
-  parent.regulateOverflow = function(element){
-    var updateOverflow = createOverflowUpdater(element);
+  parent.regulateOverflow = function(element, options){
+    var updateOverflow = createOverflowUpdater(element, options);
     
     updateOverflow();
     tower.subscribe('windowResize')(updateOverflow);
