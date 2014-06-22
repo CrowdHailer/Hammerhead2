dd852d5637f288be20fb21eafa4a7278d28e3a84
Peter <peterhsaxton@gmail.com>
2014-06-22 07:50:24 +0100

    moved scale to correct scope

--- dist/HammerHead2.js
+++ dist/HammerHead2.js
@@ -367,23 +367,34 @@ var Hammerhead = {};
   };
 }(Hammerhead));
 (function(parent){
-  var tower = Belfry.getTower(), scale;
+  var tower = Belfry.getTower();
 
   var alertStart = tower.publish('start');
   var alertPinch = tower.publish('pinch');
   var alertEnd = tower.publish('end');
 
-  var finishScrolling = _.debounce(200)(function(){
-    alertEnd('wheel');
-    scale = null;
-  });
+
+  function checkSVGTarget(svg){
+    return function(event){
+      return (event.target.ownerSVGElement || event.target) === svg;
+    };
+  }
+
   parent.mousewheelDispatch = function($element){
-    var SVGElement = $element[0];
+    var SVGElement = $element[0], scale;
+    var onTarget = checkSVGTarget(SVGElement);
+
+    var finishScrolling = _.debounce(200)(function(){
+      alertEnd('wheel');
+      scale = null;
+    });
+
     $(document).on('mousewheel', function(event){
       if (!scale) {
         scale = 1;
         console.log((event.target.ownerSVGElement || event.target) === SVGElement );
         alertStart('wheel');
+        console.log(onTarget(event));
       }
       if (event.wheelDelta > 0) {
         scale *= 1.1;
