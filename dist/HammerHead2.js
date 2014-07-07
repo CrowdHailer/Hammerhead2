f425bf02415565a9b97c581fb7e070231e5a5541
Peter <peterhsaxton@gmail.com>
2014-07-07 19:19:28 +0100

    cleaned final file

--- dist/HammerHead2.js
+++ dist/HammerHead2.js
@@ -156,25 +156,24 @@ var Hammerhead = {};
   var marginTemp = interpolate('-%(height)spx -%(width)spx');
 
   parent.regulateOverflow = function(){
-    var surplus = this.getConfig('overflowSurplus');
-    var factor = 2 * surplus + 1;
-    var $element = this.$element;
-    var $parent = $element.parent();
-    var height = $parent.height();
-    var width = $parent.width();
-    $element
-      .css('margin', marginTemp({height: height * surplus, width: width * surplus}))
-      .width(width * factor)
-      .height(height * factor);
+    var surplus = this.getConfig('overflowSurplus'),
+      factor = 2 * surplus + 1,
+      $element = this.$element,
+      $parent = $element.parent();
+
     var update = function(){
-      var height = $parent.height();
-      var width = $parent.width();
+      var height = $parent.height(),
+        width = $parent.width();
+
       $element
         .css('margin', marginTemp({height: height * surplus, width: width * surplus}))
         .width(width * factor)
         .height(height * factor);
     }
+    update();
+
     bean.on(window, 'resize', update);
+
     return function(){
       bean.off(window, 'resize', update);
     };
