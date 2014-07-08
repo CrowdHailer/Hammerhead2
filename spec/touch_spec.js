63877a50c19bc6ee5aee1f7f92831e22a9e94557
Peter <peterhsaxton@gmail.com>
2014-07-08 08:04:09 +0100

    bean setup

--- spec/touch_spec.js
+++ spec/touch_spec.js
@@ -2,8 +2,10 @@ describe('notification of gestures', function(){
   'use strict';
   // nothing for touch release without pinch or drag
   // use last drag or pinch data
+  // might need throttling but animframe could be acceptable
 
-  var $svg, $path, defaultGesture, remove;
+  var $svg, $path, defaultGesture, remove,
+    onDisplace, onInflate, onTranslate, onMagnify;
   var hammertime = Hammer(document);
   beforeEach(function(){
     $(document.body).append('<svg id="test" width="500" viewBox="0 0 2000 1000"><path id="test-path"></path></svg>');
@@ -17,6 +19,14 @@ describe('notification of gestures', function(){
         return el === $path[0];
       }
     });
+    onDisplace = jasmine.createSpy();
+    onInflate = jasmine.createSpy();
+    onTranslate = jasmine.createSpy();
+    onMagnify = jasmine.createSpy();
+    bean.on($svg[0], 'displace', onDisplace);
+    bean.on($svg[0], 'inflate', onInflate);
+    bean.on($svg[0], 'translate', onTranslate);
+    bean.on($svg[0], 'magnify', onMagnify);
   });
 
   afterEach(function(){
@@ -24,50 +34,36 @@ describe('notification of gestures', function(){
     $svg.remove();
   });
 
-  describe('on target gestures', function(){
-    var dummy;
-    beforeEach(function(){
-      dummy = jasmine.createSpy();
-    });
-
-    it('should displace on acive element', function(){
-      bean.on($svg[0], 'displace', dummy);
-      hammertime.trigger('touch', defaultGesture);
-      hammertime.trigger('drag', defaultGesture);
-      expect(dummy).toHaveBeenCalled();
-    });
-
-    it('should inflate on acive element', function(){
-      bean.on($svg[0], 'inflate', dummy);
-      hammertime.trigger('touch', defaultGesture);
-      hammertime.trigger('pinch', defaultGesture);
-      expect(dummy).toHaveBeenCalled();
-    });
-
-    it('should translate after dragging', function(){
-      bean.on($svg[0], 'translate', dummy);
-      hammertime.trigger('touch', defaultGesture);
-      hammertime.trigger('drag', defaultGesture);
-      hammertime.trigger('release', defaultGesture);
-      expect(dummy).toHaveBeenCalled();
-    });
-
-    it('should magnify after pinch', function(){
-      bean.on($svg[0], 'magnify', dummy);
-      hammertime.trigger('touch', defaultGesture);
-      hammertime.trigger('pinch', defaultGesture);
-      hammertime.trigger('release', defaultGesture);
-      expect(dummy).toHaveBeenCalled();
-    });
-
-    it('should not call without pinch or drag', function(){
-      bean.on($svg[0], 'translate', dummy);
-      bean.on($svg[0], 'magnify', dummy);
-      hammertime.trigger('touch', defaultGesture);
-      hammertime.trigger('release', defaultGesture);
-      expect(dummy).not.toHaveBeenCalled();
-    });
+  it('should displace on acive element', function(){
+    hammertime.trigger('touch', defaultGesture);
+    hammertime.trigger('drag', defaultGesture);
+    expect(onDisplace).toHaveBeenCalled();
   });
 
+  it('should inflate on acive element', function(){
+    hammertime.trigger('touch', defaultGesture);
+    hammertime.trigger('pinch', defaultGesture);
+    expect(onInflate).toHaveBeenCalled();
+  });
 
+  it('should translate after dragging', function(){
+    hammertime.trigger('touch', defaultGesture);
+    hammertime.trigger('drag', defaultGesture);
+    hammertime.trigger('release', defaultGesture);
+    expect(onTranslate).toHaveBeenCalled();
+  });
+
+  it('should magnify after pinch', function(){
+    hammertime.trigger('touch', defaultGesture);
+    hammertime.trigger('pinch', defaultGesture);
+    hammertime.trigger('release', defaultGesture);
+    expect(onMagnify).toHaveBeenCalled();
+  });
+
+  it('should not call without pinch or drag', function(){
+    hammertime.trigger('touch', defaultGesture);
+    hammertime.trigger('release', defaultGesture);
+    expect(onTranslate).not.toHaveBeenCalled();
+    expect(onMagnify).not.toHaveBeenCalled();
+  });
 });
