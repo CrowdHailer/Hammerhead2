a2ff189729480a8790d40de03fa5118966fd6641
Peter <peterhsaxton@gmail.com>
2014-07-15 17:24:55 +0100

    am added clean task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,17 @@
-module.exports = function(grunt) {
+'use strict';
 
+module.exports = function(grunt) {
+    
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+    
+    grunt.initConfig({
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                src: 'dist'
+            }
+        }
+    });
 };
