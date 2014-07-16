279db4fca892b61ac6e846015d3e8903a50fa491
Peter <peterhsaxton@gmail.com>
2014-07-16 23:02:03 +0100

    viewbox fixed

--- src/viewbox.js
+++ src/viewbox.js
@@ -1,69 +1,69 @@
 /* global Hammerhead, _, SVGroovy*/
 
 (function(parent){
-  'use strict';
+    'use strict';
 
-  var Pt = SVGroovy.Point;
-  function create(minimal, maximal){
-    if (typeof minimal === 'string') { return createFromString(minimal); }
-    return {minimal: minimal, maximal: maximal};
-  }
+    var Pt = SVGroovy.Point;
+    function create(minimal, maximal){
+        if (typeof minimal === 'string') { return createFromString(minimal); }
+        return {minimal: minimal, maximal: maximal};
+    }
 
-  function createFromString(viewBoxString){
-    function returnInt(string){ return parseInt(string, 10); }
+    function createFromString(viewBoxString){
+        function returnInt(string){ return parseInt(string, 10); }
 
-    var limits  = viewBoxString.split(' ').map(returnInt),
-        minimal = Pt(limits[0], limits[1]),
-        delta   = Pt(limits[2], limits[3]),
-        maximal = Pt.add(minimal)(delta);
-    return create(minimal, maximal);
-  }
+        var limits  = viewBoxString.split(' ').map(returnInt),
+            minimal = Pt(limits[0], limits[1]),
+            delta   = Pt(limits[2], limits[3]),
+            maximal = Pt.add(minimal)(delta);
+        return create(minimal, maximal);
+    }
 
-  function attrString(view){
-    var x0 = view.minimal.x, y0 = view.minimal.y, x1 = view.maximal.x, y1 = view.maximal.y;
-    var values = [x0, y0, x1-x0, y1-y0];
-    return values.join(' ');
-  }
+    function attrString(view){
+        var x0 = view.minimal.x, y0 = view.minimal.y, x1 = view.maximal.x, y1 = view.maximal.y;
+        var values = [x0, y0, x1-x0, y1-y0];
+        return values.join(' ');
+    }
 
-  function midpoint(view){
-    return Pt.scalar(0.5)(Pt.add(view.minimal)(view.maximal));
-  }
+    function midpoint(view){
+        return Pt.scalar(0.5)(Pt.add(view.minimal)(view.maximal));
+    }
 
-  function translate(delta){
-    return function(view){
-      var transformAll = _.map(Pt.negate(delta));
-      return transformAll(view);
-    };
-  }
+    function translate(delta){
+        return function(view){
+            var transformAll = _.map(Pt.negate(delta));
+            return transformAll(view);
+        };
+    }
 
-  function scale(factor){
-    return function(view){
-      var transformAll = _.map(Pt.scalar(1.0/factor));
-      return transformAll(view);
-    };
-  }
+    function scale(factor){
+        return function(view){
+            var transformAll = _.map(Pt.scalar(1.0/factor));
+            return transformAll(view);
+        };
+    }
 
-  function zoom(magnification){
-    return function(center){
-      return function(view){
-        center = center || midpoint(view);
-        return _.compose(
-          translate(Pt.negate(center)()),
-          scale(magnification),
-          translate(center)
-        )(view);
-      };
-    };
-  }
+    function zoom(magnification){
+        return function(center){
+            return function(view){
+                center = center || midpoint(view);
+                return _.compose(
+                  translate(Pt.negate(center)()),
+                  scale(magnification),
+                  translate(center)
+                )(view);
+            };
+        };
+    }
 
-  var extendMethods = _.extend({
-    midpoint: midpoint,
-    translate: translate,
-    scale: scale,
-    zoom: zoom,
-    attrString: attrString
-  });
+    var extendMethods = _.extend({
+        midpoint: midpoint,
+        translate: translate,
+        scale: scale,
+        zoom: zoom,
+        attrString: attrString
+    });
 
-  extendMethods(create);
-  parent.ViewBox = create;
+    extendMethods(create);
+    parent.ViewBox = create;
 }(Hammerhead));
