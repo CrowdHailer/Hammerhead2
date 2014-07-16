7df020dbaa5f820e11dfe3f0d26564974be9e4d2
Peter <peterhsaxton@gmail.com>
2014-07-16 23:04:23 +0100

    overflow fixed

--- src/overflow.js
+++ src/overflow.js
@@ -1,33 +1,34 @@
 /* global Hammerhead, bean, interpolate*/
 
 (function(parent){
-  'use strict';
+    'use strict';
 
-  var marginTemp = interpolate('-%(height)spx -%(width)spx');
+    var marginTemp = interpolate('-%(height)spx -%(width)spx');
 
-  parent.regulateOverflow = function(){
-    var surplus = this.getConfig('overflowSurplus'),
-      factor = 2 * surplus + 1,
-      $element = this.$element,
-      $parent = $element.parent();
+    parent.regulateOverflow = function(){
+        var surplus = this.getConfig('overflowSurplus'),
+            factor = 2 * surplus + 1,
+            $element = this.$element,
+            $parent = $element.parent();
 
-    var update = function(){
-      var height = $parent.height(),
-        width = $parent.width();
+        var update = function(){
+            var height = $parent.height(),
+                width = $parent.width();
 
-      $element
-        .css('margin', marginTemp({height: height * surplus, width: width * surplus}))
-        .width(width * factor)
-        .height(height * factor);
+            $element
+                .css('margin', marginTemp({height: height * surplus, width: width * surplus}))
+                .width(width * factor)
+                .height(height * factor);
+        };
+        
+        update();
+        setTimeout(update, 1000); //Android fix as early window sizes are incorrect
+
+        bean.on(window, 'resize', update);
+
+        return function(){
+            bean.off(window, 'resize', update);
+        };
     };
-    update();
-    setTimeout(update, 1000); //Android fix as easrly window sizes are incorrect
-
-    bean.on(window, 'resize', update);
-
-    return function(){
-      bean.off(window, 'resize', update);
-    };
-  };
 
 }(Hammerhead));
